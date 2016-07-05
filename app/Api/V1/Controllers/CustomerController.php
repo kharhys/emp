fc43a26af88b7ea88f422a9ea3c0ecadeaf21316
kharhys <kelgitonga@gmail.com>
2016-07-05 14:04:25 +0300

    fix autoload

--- app/Api/V1/Controllers/CustomerController.php
+++ app/Api/V1/Controllers/CustomerController.php
@@ -1,12 +1,41 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Api\V1\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+
+use JWTAuth;
+use App\Customer;
+use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller;
 
 class CustomerController extends Controller
 {
-    //
+    //attach Helpers
+    use Helpers;
+
+    public function index()
+    {
+        return Customer::all()->toArray();
+    }
+
+    public function store(Request $request)
+    {
+        $currentUser = JWTAuth::parseToken()->authenticate();
+
+        $customer = new Customer;
+
+        $customer->name = $request->get('name');
+        $customer->gender = $request->get('gender');
+        $customer->occupation = $request->get('occupation');
+        $customer->address = $request->get('address');
+        $customer->phone = $request->get('phone');
+        $customer->dob = $request->get('dob');
+
+        if($customer->save())
+            return $this->response->created();
+        else
+            return $this->response->error('could_not_create_customer', 500);
+    }
 }
