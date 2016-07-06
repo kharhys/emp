cbcce0749cad31e8266fd0175395dec65a7cef7a
kharhys <kelgitonga@gmail.com>
2016-07-06 15:55:51 +0300

    with create, read

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,9 +132,56 @@
           <div id="<?= 'modal'.$customer['id']; ?>" class="modal modal__bg" role="dialog" aria-hidden="true">
             <div class="modal__dialog">
               <div class="modal__content">
-                <h1>Update / Delete Customer</h1>
                 <p style="color: #191919">
-                  form here. button there.
+                  <form class="form" onsubmit="updateCustomer(event);" id="create-form">
+                    <h1>Update / Delete Customer</h1>
+
+                    <fieldset class="form-fieldset ui-input __first">
+                      <input type="text" id="name" name="name" tabindex="0" />
+                      <label for="name">
+                        <span data-text="Name">Name</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __second">
+                      <input type="text" id="gender"  name="gender" tabindex="0" />
+                      <label for="gender">
+                        <span data-text="Gender">Gender</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __third">
+                      <input type="text" id="occupation" name="occupation" />
+                      <label for="occupation">
+                        <span data-text="Occupation">Occupation</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __fourth">
+                      <input type="text" id="address"  name="address" />
+                      <label for="address">
+                        <span data-text="Address">Address</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __fourth">
+                      <input type="text" id="phone"  name="phone" />
+                      <label for="phone">
+                        <span data-text="Phone">Phone</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __fourth">
+                      <input type="text" id="dob"  name="dob" />
+                      <label for="dob">
+                        <span data-text="Date of Birth">Date of Birth</span>
+                      </label>
+                    </fieldset>
+
+                    <div class="form-footer">
+                      <input type="submit" class="btn" value="Submit" />
+                    </div>
+                  </form>
                 </p>
 
                 <!-- modal close button -->
