66e58101304855ff9a5a6cd8f2fc57ae4f1eb303
kharhys <kelgitonga@gmail.com>
2016-07-06 16:00:04 +0300

    with create, read

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,63 +135,65 @@
                 <h1>Update / Delete Customer</h1>
                 <p style="color: #191919">
 
-                  <div class="bar">
-                    <button class="active"><i class="material-icons fab active">cancel</i></button>
-                    <h2 class="mdl-card__title-text">Create new customer</h2>
-                  </div>
-                  <div class="content">
-                    <div class="form-container" >
-                      <form class="form" onsubmit="createCustomer(event);" id="create-form">
+                  <div class="fab-modal-modal-content">
+                    <div class="bar">
+                      <button class="active"><i class="material-icons fab active">cancel</i></button>
+                      <h2 class="mdl-card__title-text">Create new customer</h2>
+                    </div>
+                    <div class="content">
+                      <div class="form-container" >
+                        <form class="form" onsubmit="createCustomer(event);" id="create-form">
 
-                        <fieldset class="form-fieldset ui-input __first">
-                          <input type="text" id="name" name="name" tabindex="0" />
-                          <label for="name">
-                            <span data-text="Name">Name</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __first">
+                            <input type="text" id="name" name="name" tabindex="0" />
+                            <label for="name">
+                              <span data-text="Name">Name</span>
+                            </label>
+                          </fieldset>
 
-                        <fieldset class="form-fieldset ui-input __second">
-                          <input type="text" id="gender"  name="gender" tabindex="0" />
-                          <label for="gender">
-                            <span data-text="Gender">Gender</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __second">
+                            <input type="text" id="gender"  name="gender" tabindex="0" />
+                            <label for="gender">
+                              <span data-text="Gender">Gender</span>
+                            </label>
+                          </fieldset>
 
-                        <fieldset class="form-fieldset ui-input __third">
-                          <input type="text" id="occupation" name="occupation" />
-                          <label for="occupation">
-                            <span data-text="Occupation">Occupation</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __third">
+                            <input type="text" id="occupation" name="occupation" />
+                            <label for="occupation">
+                              <span data-text="Occupation">Occupation</span>
+                            </label>
+                          </fieldset>
 
-                        <fieldset class="form-fieldset ui-input __fourth">
-                          <input type="text" id="address"  name="address" />
-                          <label for="address">
-                            <span data-text="Address">Address</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __fourth">
+                            <input type="text" id="address"  name="address" />
+                            <label for="address">
+                              <span data-text="Address">Address</span>
+                            </label>
+                          </fieldset>
 
-                        <fieldset class="form-fieldset ui-input __fourth">
-                          <input type="text" id="phone"  name="phone" />
-                          <label for="phone">
-                            <span data-text="Phone">Phone</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __fourth">
+                            <input type="text" id="phone"  name="phone" />
+                            <label for="phone">
+                              <span data-text="Phone">Phone</span>
+                            </label>
+                          </fieldset>
 
-                        <fieldset class="form-fieldset ui-input __fourth">
-                          <input type="text" id="dob"  name="dob" />
-                          <label for="dob">
-                            <span data-text="Date of Birth">Date of Birth</span>
-                          </label>
-                        </fieldset>
+                          <fieldset class="form-fieldset ui-input __fourth">
+                            <input type="text" id="dob"  name="dob" />
+                            <label for="dob">
+                              <span data-text="Date of Birth">Date of Birth</span>
+                            </label>
+                          </fieldset>
 
-                        <div class="form-footer">
-                          <input type="submit" class="btn" value="Submit" />
-                        </div>
-                      </form>
+                          <div class="form-footer">
+                            <input type="submit" class="btn" value="Submit" />
+                          </div>
+                        </form>
+                      </div>
                     </div>
                   </div>
-                  
+
                 </p>
 
                 <!-- modal close button -->
