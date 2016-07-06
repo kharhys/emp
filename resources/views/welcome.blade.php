52ae611aad5921d82619cc3fc133f11ed83e3e52
kharhys <kelgitonga@gmail.com>
2016-07-06 12:12:14 +0300

    with form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,45 @@
           <div class="modal__dialog">
             <div class="modal__content">
               <h1>New Customer</h1>
-              <p>some form here</p>
+              <p>
+                <div class="form-container" >
+                  <h2>Create new customer</h2>
+                  <form class="form" action="#">
+
+                    <fieldset class="form-fieldset ui-input __first">
+                      <input type="text" id="username" tabindex="0" />
+                      <label for="username">
+                        <span data-text="Username">Username</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __second">
+                      <input type="email" id="email" tabindex="0" />
+                      <label for="email">
+                        <span data-text="E-mail Address">E-mail Address</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __third">
+                      <input type="password" id="new-password" />
+                      <label for="new-password">
+                        <span data-text="New Password">New Password</span>
+                      </label>
+                    </fieldset>
+
+                    <fieldset class="form-fieldset ui-input __fourth">
+                      <input type="password" id="repeat-new-password" />
+                      <label for="repeat-new-password">
+                        <span data-text="Repeat New Password">Repeat New Password</span>
+                      </label>
+                    </fieldset>
+
+                    <div class="form-footer">
+                      <input type="submit" class="btn" value="Submit" />
+                    </div>
+                  </form>
+                </div>
+              </p>
 
               <!-- modal close button -->
               <a href="" class="modal__close demo-close">
@@ -385,7 +423,7 @@
       animating = false;
     }));
   }
-
+  
   $('.toggle').click(function () {
     if ($('.list').hasClass('hidden')) {
       animateEnter();
@@ -398,5 +436,11 @@
 
   // for dramatic effect :P
   setTimeout(animateEnter, 1000);
+
+  var $input = $('.form-fieldset > input');
+
+  $input.blur(function (e) {
+    $(this).toggleClass('filled', !!$(this).val());
+  });
   </script>
   </html>
