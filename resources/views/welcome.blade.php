76526b6bc500438e6486bb3cee4d0fb43942743d
kharhys <kelgitonga@gmail.com>
2016-07-06 16:29:58 +0300

    with create, read

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,67 +132,63 @@
           <div id="<?= 'modal'.$customer['id']; ?>" class="modal modal__bg" role="dialog" aria-hidden="true">
             <div class="modal__dialog">
               <div class="modal__content">
-                <p style="color: #191919; margin-top: 2rem;">
+                <div class="fab-modal-modal-content" style="width: auto; color: #191919; margin-top: 2rem;">
+                  <div class="bar" style="padding: 1rem;">
+                    <h2 class="mdl-card__title-text">Update Customer</h2>
+                  </div>
+                  <div class="content" style="overflow-y: auto;">
+                    <div class="form-container" >
+                      <form class="form" onsubmit="updateCustomer(event);" id="create-form">
 
-                  <div class="fab-modal-modal-content">
-                    <div class="bar" style="padding: 1rem;">
-                      <h2 class="mdl-card__title-text">Update Customer</h2>
-                    </div>
-                    <div class="content" style="overflow-y: auto;">
-                      <div class="form-container" >
-                        <form class="form" onsubmit="updateCustomer(event);" id="create-form">
+                        <fieldset class="form-fieldset ui-input __first">
+                          <input type="text" id="name" name="name" tabindex="0" value="<?=$customer['name']?>" />
+                          <label for="name">
+                            <span data-text="Name">Name</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __first">
-                            <input type="text" id="name" name="name" tabindex="0" value="<?=$customer['name']?>" />
-                            <label for="name">
-                              <span data-text="Name">Name</span>
-                            </label>
-                          </fieldset>
+                        <fieldset class="form-fieldset ui-input __second">
+                          <input type="text" id="gender"  name="gender" tabindex="0" value="<?=$customer['gender']?>" />
+                          <label for="gender">
+                            <span data-text="Gender">Gender</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __second">
-                            <input type="text" id="gender"  name="gender" tabindex="0" value="<?=$customer['gender']?>" />
-                            <label for="gender">
-                              <span data-text="Gender">Gender</span>
-                            </label>
-                          </fieldset>
+                        <fieldset class="form-fieldset ui-input __third">
+                          <input type="text" id="occupation" name="occupation" value="<?=$customer['occupation']?>" />
+                          <label for="occupation">
+                            <span data-text="Occupation">Occupation</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __third">
-                            <input type="text" id="occupation" name="occupation" value="<?=$customer['occupation']?>" />
-                            <label for="occupation">
-                              <span data-text="Occupation">Occupation</span>
-                            </label>
-                          </fieldset>
+                        <fieldset class="form-fieldset ui-input __fourth">
+                          <input type="text" id="address"  name="address" value="<?=$customer['address']?>" />
+                          <label for="address">
+                            <span data-text="Address">Address</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __fourth">
-                            <input type="text" id="address"  name="address" value="<?=$customer['address']?>" />
-                            <label for="address">
-                              <span data-text="Address">Address</span>
-                            </label>
-                          </fieldset>
+                        <fieldset class="form-fieldset ui-input __fourth">
+                          <input type="text" id="phone"  name="phone" value="<?=$customer['phone']?>" />
+                          <label for="phone">
+                            <span data-text="Phone">Phone</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __fourth">
-                            <input type="text" id="phone"  name="phone" value="<?=$customer['phone']?>" />
-                            <label for="phone">
-                              <span data-text="Phone">Phone</span>
-                            </label>
-                          </fieldset>
+                        <fieldset class="form-fieldset ui-input __fourth">
+                          <input type="text" id="dob"  name="dob" value="<?=$customer['dob']?>" />
+                          <label for="dob">
+                            <span data-text="Date of Birth">Date of Birth</span>
+                          </label>
+                        </fieldset>
 
-                          <fieldset class="form-fieldset ui-input __fourth">
-                            <input type="text" id="dob"  name="dob" value="<?=$customer['dob']?>" />
-                            <label for="dob">
-                              <span data-text="Date of Birth">Date of Birth</span>
-                            </label>
-                          </fieldset>
-
-                          <div class="form-footer">
-                            <input type="submit" class="btn" value="Submit" />
-                          </div>
-                        </form>
-                      </div>
+                        <div class="form-footer">
+                          <input type="submit" class="btn" value="Submit" />
+                        </div>
+                      </form>
                     </div>
                   </div>
-
-                </p>
+                </div>
 
                 <!-- modal close button -->
                 <a href="" class="modal__close demo-close">
