63c443d84c01b5cd807a91bbe38236c481cd2127
kharhys <kelgitonga@gmail.com>
2016-07-06 15:36:19 +0300

    DISABLE AUTH TEMPORARILY

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                       </div>
                     </div>
                   </span>
-                  <span data-modal="#modal" class="modal__trigger">
+                  <span data-modal=<?= "modal".$customer['id']; ?> class="modal__trigger">
                     <i class="material-icons">more_vert</i>
                   </span>
                 </li>
@@ -145,23 +145,25 @@
         </div>
 
 
-        <!-- Modal -->
-        <div id="modal" class="modal modal__bg" role="dialog" aria-hidden="true">
-          <div class="modal__dialog">
-            <div class="modal__content">
-              <h1>New Customer</h1>
-              <p>
-                something
-              </p>
+        <?php foreach ($customers as $customer): ?>
+          <!-- Modal -->
+          <div id=<?= "modal".$customer['id']; ?> class="modal modal__bg" role="dialog" aria-hidden="true">
+            <div class="modal__dialog">
+              <div class="modal__content">
+                <h1>New Customer</h1>
+                <p>
+                  something
+                </p>
 
-              <!-- modal close button -->
-              <a href="" class="modal__close demo-close">
-                <svg class="" viewBox="0 0 24 24"><path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"/><path d="M0 0h24v24h-24z" fill="none"/></svg>
-              </a>
+                <!-- modal close button -->
+                <a href="" class="modal__close demo-close">
+                  <svg class="" viewBox="0 0 24 24"><path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"/><path d="M0 0h24v24h-24z" fill="none"/></svg>
+                </a>
 
+              </div>
             </div>
           </div>
-        </div>
+        <?php endforeach; ?>
 
         <div id="modal2" class="modal modal--align-top modal__bg" role="dialog" aria-hidden="true">
           <div class="modal__dialog">
