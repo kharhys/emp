0e9b68ce1b4a3e49f8bd20ab83c19a39608379b3
kharhys <kelgitonga@gmail.com>
2016-07-06 15:18:42 +0300

    DISABLE AUTH TEMPORARILY

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,6 @@
 |
 */
 
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-
-Route::get('/', 'App\Api\V1\Controllers\CustomerController@welcome');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
+<?php
+use App\Customer;
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
