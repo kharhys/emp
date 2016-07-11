f5ea548b939b8cd19053734b18a9bd2feffb0d6c
kharhys <kelgitonga@gmail.com>
2016-07-11 15:21:13 +0300

    site controller

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,12 +9,16 @@ use App\Http\Controllers\Controller;
 class SiteController extends Controller {
 
     public function index(Request $request) {
+      return view('landing');
+    }
+
+    public function customers(Request $request) {
       $input = $request->getQueryString();
       $phone = explode("=", $input)[1];
 
       $customer = Customer::where('phone', $phone)->first();
       $customers = Customer::where('phone', '<>', $phone)->get()->toArray();
-      
+
       return view('welcome', [ 'customers' => $customers, 'customer' => $customer]);
     }
 
---
+++ resources/views/head.blade.php
@@ -0,0 +1,9 @@
+<head>
+  <title>Customer Management Portal</title>
+
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <link href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+  <link href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css" rel="stylesheet" type="text/css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+</head>
