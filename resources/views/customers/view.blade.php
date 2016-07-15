8763e4b24da09c6334f63e3d4f53bd7ec7f41d2e
kharhys <kelgitonga@gmail.com>
2016-07-15 16:22:57 +0300

    customers view

--- resources/views/customers/view.blade.php
+++ resources/views/customers/view.blade.php
@@ -7,6 +7,21 @@
 @section('page')
 <div class="container">
   <div class="main">
+    <header class="page-header">
+      <h1> View Customer  </h1>
+    </header>
+
+    <div class="fab-modal-backdrop">
+      <div class="fab-modal-shadow-surogate"></div>
+      <div class="fab-modal-container inactive" id="dialog">
+        <div class="fab-modal-inner">
+          <button class="inactive dialog__btn">
+            <a href="{{ URL::to('customers') }}"> <i class="material-icons fab inactive">close</i> </a>
+          </button>
+        </div>
+      </div>
+    </div>
+
     <div class="page_card">
 
       <div class="page_card_header">
