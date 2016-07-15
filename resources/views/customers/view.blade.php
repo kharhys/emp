146fb848ddec7b0049ab16e43e190d46a8017c0f
kharhys <kelgitonga@gmail.com>
2016-07-15 15:16:52 +0300

    customers view

--- resources/views/customers/view.blade.php
+++ resources/views/customers/view.blade.php
@@ -6,30 +6,6 @@
 
 @section('page')
 <div class="container">
-  <header class="page-header">
-    <h1> View Customer  </h1>
-    <?php print_r($customer); ?>
-  </header>
-
-  <div class="fab-modal-backdrop">
-    <div class="fab-modal-shadow-surogate"></div>
-    <div class="fab-modal-container inactive" id="dialog">
-      <div class="fab-modal-inner">
-        <button class="inactive dialog__btn"><i class="material-icons fab inactive">add</i></button>
-        <div class="fab-modal-modal-content">
-          <div class="bar">
-            <button class="active dialog__btn"><i class="material-icons fab active">cancel</i></button>
-            <h2 class="mdl-card__title-text">View Customer</h2>
-          </div>
-          <div class="content">
-            <div class="form-container" >
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div class="main">
     <div class="view__card">
       <h1>Customer Details</h1>
