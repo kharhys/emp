3ba4df54b000d364b1b3f645eda4843345e94ed2
kharhys <kelgitonga@gmail.com>
2016-07-15 16:14:47 +0300

    customers view

--- resources/views/customers/view.blade.php
+++ resources/views/customers/view.blade.php
@@ -7,40 +7,93 @@
 @section('page')
 <div class="container">
   <div class="main">
-    <div class="view__card">
-      <h1>Customer Details</h1>
-      <div class="tab-wrap">
+    <div class="page_card">
 
-        <input type="radio" name="tabs" id="tab1" checked>
-        <div class="tab-label-content" id="tab1-content">
-          <label for="tab1">Tab 1</label>
-          <div class="tab-content">TAB 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet eget lectus eu congue. Nam finibus urna eget nisl aliquam, in dictum ligula feugiat. Donec mollis ligula purus, et interdum velit bibendum eget. Aliquam magna diam, tristique eu libero nec, sagittis finibus sapien. Cras a ex ultricies, faucibus elit sagittis, maximus nisi. Donec quis arcu sapien. Aenean risus nibh, varius sed porttitor a, ornare nec leo. Sed vitae lacus in ipsum varius sagittis. Ut in quam cursus, ullamcorper sapien posuere, laoreet elit. Suspendisse interdum, risus ut ultricies scelerisque, nibh est commodo leo, sed tristique nisl odio et turpis. Fusce pellentesque nunc nec arcu feugiat accumsan. Praesent mauris sem, eleifend sit amet tortor in, cursus vehicula arcu. Curabitur convallis sit amet nunc ac feugiat. Sed at risus id diam porta pretium id vel felis. Donec nec dui id nisl hendrerit laoreet eu id odio.</div>
+      <div class="page_card_header">
+        <div id="customer_details">
+          <h1> Customer Name </h1>
+          <h2> Phone Number </h2>
         </div>
-
-        <input type="radio" name="tabs" id="tab2">
-        <div class="tab-label-content" id="tab2-content">
-          <label for="tab2">Tab 2</label>
-          <div class="tab-content">TAB 2 - Quisque egestas, purus in tempor vulputate, diam augue mollis quam, quis elementum ipsum ex a risus. Quisque sed augue porta, facilisis felis vitae, cursus mi. Nullam mollis magna eget tincidunt mollis. Sed suscipit placerat ultricies. Sed eget lorem et ipsum ultricies congue eu a enim. Nam quis ex nec lorem dignissim suscipit eu ut felis. Vivamus molestie felis id purus congue, vel ultrices sem molestie.</div>
+        <div>
+          <ul class="nav">
+            <div class="kebab">
+              <figure></figure>
+              <figure class="middle"></figure>
+              <p class="cross">x</p>
+              <figure></figure>
+              <ul class="m-dropdown">
+                <li><a href="http://www.g.com">Art</a></li>
+                <li><a href="http://www.g.com">Coding</a></li>
+                <li><a href="http://www.g.com">Design</a></li>
+                <li><a href="http://www.g.com">Web Development</a></li>
+              </ul>
+            </div>
+          </ul>
         </div>
-
-        <input type="radio" name="tabs" id="tab3">
-        <div class="tab-label-content" id="tab3-content">
-          <label for="tab3">Tab 3</label>
-          <div class="tab-content">TAB 3 - Donec vulputate ante ac ligula vestibulum, id mollis diam commodo. Integer at consequat magna. Sed elit sem, dictum nec porttitor ac, ultrices id enim. Morbi semper eros a enim malesuada, eu finibus erat dictum. Ut vitae orci a odio sagittis malesuada. Cras volutpat vel lorem in tempor. Duis ultricies lectus sit amet tellus vehicula faucibus. Etiam sed leo ac erat tempor feugiat at quis ipsum. Mauris pellentesque nisl lorem, ac finibus sapien sagittis vel. Donec et lobortis est. Vestibulum dignissim ligula nec erat interdum, quis sollicitudin metus pretium. Vestibulum quis dui sapien. Proin commodo justo ac orci elementum molestie. Aliquam mattis orci vitae volutpat commodo.</div>
-        </div>
-
-        <input type="radio" name="tabs" id="tab4">
-        <div class="tab-label-content" id="tab4-content">
-          <label for="tab4">Tab 4</label>
-          <div class="tab-content">TAB 4 - Quisque egestas, purus in tempor vulputate, diam augue mollis quam, quis elementum ipsum ex a risus. Quisque sed augue porta, facilisis felis vitae, cursus mi. Nullam mollis magna eget tincidunt mollis. Sed suscipit placerat ultricies. Sed eget lorem et ipsum ultricies congue eu a enim. Nam quis ex nec lorem dignissim suscipit eu ut felis. Vivamus molestie felis id purus congue, vel ultrices sem molestie.</div>
-        </div>
-
-        <div class="slide"></div>
-
       </div>
-    </div>
-  </div>
+      <main>
 
+        <input id="tab1" type="radio" name="tabs" checked>
+        <label for="tab1">Personal Details</label>
+
+        <input id="tab2" type="radio" name="tabs">
+        <label for="tab2">Property Details</label>
+
+        <input id="tab3" type="radio" name="tabs">
+        <label for="tab3">Attached Documents</label>
+
+        <section id="content1">
+          <p>
+            <dl>
+              <dt>Name</dt>
+              <dd>Sally Jones</dd>
+
+              <dt>Occupation</dt>
+              <dd>chef</dd>
+
+              <dt>Address</dt>
+              <dd>521 First Street, Chicago IL, USA</dd>
+
+              <dt>This is really long</dt>
+              <dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</dd>
+
+              <dt>Definition term</dt>
+              <dd>Definition definition</dd>
+            </dl>
+          </p>
+        </section>
+
+        <section id="content2">
+          <p>
+            Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+          </p>
+          <p>
+            Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+          </p>
+        </section>
+
+        <section id="content3">
+          <p>
+            Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+          </p>
+          <p>
+            Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+          </p>
+        </section>
+
+        <section id="content4">
+          <p>
+            Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+          </p>
+          <p>
+            Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+          </p>
+        </section>
+
+      </main>
+    </div>
+
+  </div>
 </div>
 @endsection
 
