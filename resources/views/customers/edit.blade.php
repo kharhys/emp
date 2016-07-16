d6d27559c132418076b5c62ca5f7ddd3537839e7
kharhys <kelgitonga@gmail.com>
2016-07-16 18:23:46 +0300

    customers view

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -29,43 +29,45 @@
     </div>
   </div>
 
-  <div class="main">
-    <form class = "steps-form" method="post" action="{{ URL::to('customers/update') }}" >
-      <input type="hidden" value="<?customer['phone_number']?>" name="old_phone" />
-      <div class = "step-form" data-next = ".step-form.step-two">
-        <h2>Customer Details</h2>
-        <input type="text" value="<?customer['first_name']?>" name="first_name" placeholder = "First Name" class = "form-input">
-        <input type="text" value="<?customer['last_name']?>" name="last_name" placeholder = "Last Name" class = "form-input">
-        <input type="text" value="<?customer['date_of_birth']?>" name="date_of_birth" placeholder = "Date Of Birth Name" class = "form-input">
-        <input type="text" value="<?customer['phone_number']?>" name="phone_number" placeholder = "Phone Number" class = "form-input">
-        <input type="text" value="<?customer['mobile_number']?>" name="mobile_number" placeholder = "Mobile Number" class = "form-input">
-        <input type="text" value="<?customer['nationality']?>" name="nationality" placeholder = "Nationality" class = "form-input">
-        <input type="text" value="<?customer['passport_number']?>" name="passport_number" placeholder = "Passport Number" class = "form-input">
-        <input type="text" value="<?customer['emirates_id']?>" name="emirates_id" placeholder = "Emirates ID" class = "form-input">
-        <button class = "form-input btn next-step" type = "button">Next</button>
-      </div>
-      <div class = "step-form step-two" data-next = ".step-form.step-three">
-        <h2>Property Details</h2>
-        <input type="text" name="tower_name" placeholder = "Tower Name" class = "form-input">
-        <input type="text" name="apartment_number" placeholder = "Apartment Number" class = "form-input">
-        <input type="text" name="area_sq_ft" placeholder = "Area In Sq. Ft." class = "form-input">
-        <input type="text" name="contract_date" placeholder = "Contract Date" class = "form-input">
-        <input type="text" name="address_one" placeholder = "Address 1" class = "form-input">
-        <input type="text" name="address_two" placeholder = "Address 2" class = "form-input">
-        <input type="text" name="city" placeholder = "City" class = "form-input">
-        <input type="text" name="postal_address" placeholder = "P.O. Box" class = "form-input">
-        <input type="text" name="email_address" placeholder = "Email Address" class = "form-input">
-        <button class = "form-input btn next-step" type = "button">Next</button>
-      </div>
-      <div class = "step-form step-three">
-        <h2>Attach Documents</h2>
-        <label for="occupation"> <span data-text="Occupation">Attach Passport Copy</span>  </label>
-        <input name="passport_attachment" type="file" placeholder = "" class = "form-input">
-        <label for="occupation"> <span data-text="Occupation">Attach Contract Copy</span>  </label>
-        <input name="contract_attachment" type="file" placeholder = "" class = "form-input">
-        <button type = "submit" class = "form-input btn">Submit</button>
-      </div>
-    </form>
+  <div class="table__card">
+    <div class="main">
+      <form class = "steps-form" method="post" action="{{ URL::to('customers/update') }}" >
+        <input type="hidden" value="<?customer['phone_number']?>" name="old_phone" />
+        <div class = "step-form" data-next = ".step-form.step-two">
+          <h2>Customer Details</h2>
+          <input type="text" value="<?=customer['first_name']?>" name="first_name" placeholder = "First Name" class = "form-input">
+          <input type="text" value="<?=customer['last_name']?>" name="last_name" placeholder = "Last Name" class = "form-input">
+          <input type="text" value="<?=customer['date_of_birth']?>" name="date_of_birth" placeholder = "Date Of Birth Name" class = "form-input">
+          <input type="text" value="<?=customer['phone_number']?>" name="phone_number" placeholder = "Phone Number" class = "form-input">
+          <input type="text" value="<?=customer['mobile_number']?>" name="mobile_number" placeholder = "Mobile Number" class = "form-input">
+          <input type="text" value="<?=customer['nationality']?>" name="nationality" placeholder = "Nationality" class = "form-input">
+          <input type="text" value="<?=customer['passport_number']?>" name="passport_number" placeholder = "Passport Number" class = "form-input">
+          <input type="text" value="<?=customer['emirates_id']?>" name="emirates_id" placeholder = "Emirates ID" class = "form-input">
+          <button class = "form-input btn next-step" type = "button">Next</button>
+        </div>
+        <div class = "step-form step-two" data-next = ".step-form.step-three">
+          <h2>Property Details</h2>
+          <input type="text" name="tower_name" placeholder = "Tower Name" class = "form-input">
+          <input type="text" name="apartment_number" placeholder = "Apartment Number" class = "form-input">
+          <input type="text" name="area_sq_ft" placeholder = "Area In Sq. Ft." class = "form-input">
+          <input type="text" name="contract_date" placeholder = "Contract Date" class = "form-input">
+          <input type="text" name="address_one" placeholder = "Address 1" class = "form-input">
+          <input type="text" name="address_two" placeholder = "Address 2" class = "form-input">
+          <input type="text" name="city" placeholder = "City" class = "form-input">
+          <input type="text" name="postal_address" placeholder = "P.O. Box" class = "form-input">
+          <input type="text" name="email_address" placeholder = "Email Address" class = "form-input">
+          <button class = "form-input btn next-step" type = "button">Next</button>
+        </div>
+        <div class = "step-form step-three">
+          <h2>Attach Documents</h2>
+          <label for="occupation"> <span data-text="Occupation">Attach Passport Copy</span>  </label>
+          <input name="passport_attachment" type="file" placeholder = "" class = "form-input">
+          <label for="occupation"> <span data-text="Occupation">Attach Contract Copy</span>  </label>
+          <input name="contract_attachment" type="file" placeholder = "" class = "form-input">
+          <button type = "submit" class = "form-input btn">Submit</button>
+        </div>
+      </form>
+    </div>
   </div>
 
 </div>
