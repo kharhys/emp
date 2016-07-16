8c3209e0305c87ac8d4ede7e64f9b0e8af2545bc
kharhys <kelgitonga@gmail.com>
2016-07-16 16:18:20 +0300

    create >> add

--- database/migrations/2016_07_16_113738_add_property_details_to_customers_table.php
+++ database/migrations/2016_07_16_113738_add_property_details_to_customers_table.php
@@ -13,7 +13,34 @@ class AddPropertyDetailsToCustomersTable extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            //
+            //make cols nullable
+            $table->string('passport_number')->nullable()->change();
+            $table->string('date_of_birth')->nullable()->change();
+            $table->string('mobile_number')->nullable()->change();
+            $table->string('phone_number')->nullable()->change();
+            $table->string('nationality')->nullable()->change();
+            $table->string('emirates_id')->nullable()->change();
+            $table->string('first_name')->nullable()->change();
+            $table->string('last_name')->nullable()->change();
+            $table->string('occupation')->nullable()->change();
+            $table->string('address')->nullable()->change();
+            $table->string('gender')->nullable()->change();
+            $table->string('phone')->nullable()->change();
+            $table->string('name')->nullable()->change();
+            $table->string('dob')->nullable()->change();
+            //create other cols
+            $table->string('apartment_number')->nullable();
+            $table->string('postal_address')->nullable();
+            $table->string('contract_date')->nullable();
+            $table->string('email_address')->nullable();
+            $table->string('address_one')->nullable();
+            $table->string('address_two')->nullable();
+            $table->string('area_sq_ft')->nullable();
+            $table->string('tower_name')->nullable();
+            $table->string('city')->nullable();
+
+            $table->string('passport_attachment')->nullable();
+            $table->string('contract_attachment')->nullable();
         });
     }
 
@@ -25,7 +52,20 @@ class AddPropertyDetailsToCustomersTable extends Migration
     public function down()
     {
         Schema::table('customers', function (Blueprint $table) {
-            //
+            //drop cols
+            $table->dropColumn('apartment_number');
+            $table->dropColumn('postal_address');
+            $table->dropColumn('contract_date');
+            $table->dropColumn('email_address');
+            $table->dropColumn('address_one');
+            $table->dropColumn('address_two');
+            $table->dropColumn('area_sq_ft');
+            $table->dropColumn('tower_name');
+            $table->dropColumn('city');
+
+            $table->dropColumn('passport_attachment');
+            $table->dropColumn('contract_attachment');
+
         });
     }
 }
--- resources/views/customers/view.blade.php
+++ resources/views/customers/view.blade.php
@@ -36,10 +36,10 @@
               <figure class="middle"></figure>
               <p class="cross">x</p>
               <figure></figure>
-              <?php $edit_url = \URL::to('customers/edit', [ 'phone' => $customer['phone'] ]) ?>
-              <?php $delete_url = \URL::to('customers/delete', [ 'phone' => $customer['phone'] ]) ?>
+              <?php $edit_url = \URL::to('customers/edit', [ 'phone' => $customer['phone'] ]); ?>
+              <?php $delete_url = \URL::to('customers/delete', [ 'phone' => $customer['phone'] ]); ?>
               <ul class="m-dropdown">
-                <li><a href="#">Edit</a></li>
+                <li><a href=<?=$edit_url?> >Edit</a></li>
                 <li><a href=<?=$delete_url?> >Delete</a></li>
               </ul>
             </div>
