91a3d99f30a9e08b5b969bca454eef32c4b2363e
kharhys <kelgitonga@gmail.com>
2016-07-19 11:31:20 +0300

    view popup

--- public/kebab.js
+++ public/kebab.js
@@ -5,11 +5,14 @@
     middle = document.querySelector('.middle'),
     cross = document.querySelector('.cross'),
     dropdown = document.querySelector('.m-dropdown');
+
+    if(kebab) {
+      kebab.addEventListener('click', function() {
+        middle.classList.toggle('active');
+        cross.classList.toggle('active');
+        dropdown.classList.toggle('active');
+      })
+    }
     
-    kebab.addEventListener('click', function() {
-      middle.classList.toggle('active');
-      cross.classList.toggle('active');
-      dropdown.classList.toggle('active');
-    })
   });
 }(jQuery));
